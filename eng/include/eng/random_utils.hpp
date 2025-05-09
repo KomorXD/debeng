b94ac05674cd1d2c2b2362c5174b83b75d4d9aa9
Paweł Kupczak <komorxd_t@proton.me>
2025-05-09 23:58:30 +0200

    File for random utils not related to anything bigger rly

---
+++ eng/include/eng/random_utils.hpp
@@ -0,0 +1,9 @@
+#ifndef RANDOM_UTILS_HPP
+#define RANDOM_UTILS_HPP
+
+#include <optional>
+#include <string>
+
+std::optional<std::string> get_file_content(const std::string &path);
+
+#endif
---
+++ eng/src/random_utils.cpp
@@ -0,0 +1,18 @@
+#include "random_utils.hpp"
+#include <sstream>
+#include <string>
+#include <fstream>
+
+std::optional<std::string> get_file_content(const std::string &path) {
+    std::ifstream file(path);
+    if (!file.good())
+        return {};
+
+    std::string line{};
+    std::stringstream ss{};
+    while (std::getline(file, line)) {
+        ss << line << '\n';
+    }
+
+    return ss.str();
+}

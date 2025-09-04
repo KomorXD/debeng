be2f0959c44c0385cb1ce92d70b297576d796271
Paweł Kupczak <komorxd_t@proton.me>
2025-09-05 00:00:42 +0200

    edi, layers: basic editor layer

---
+++ edi/src/editor_layer.cpp
@@ -0,0 +1,181 @@
+#include "eng/containers/registry.hpp"
+#include "eng/event.hpp"
+#include "eng/input.hpp"
+#include "eng/renderer/camera.hpp"
+#include "eng/renderer/renderer.hpp"
+#include "eng/scene/assets.hpp"
+#include "eng/scene/components.hpp"
+#include "glm/fwd.hpp"
+#include "imgui/imgui.h"
+#include "imgui/imgui_internal.h"
+#include "layers.hpp"
+
+std::unique_ptr<Layer> EditorLayer::create(const eng::WindowSpec &win_spec) {
+    glm::ivec2 window_size = glm::ivec2(win_spec.width, win_spec.height);
+
+    std::unique_ptr<EditorLayer> layer = std::make_unique<EditorLayer>();
+    layer->camera.position = glm::vec3(0.0f, 2.0f, -3.0f);
+    layer->camera.viewport = window_size;
+    layer->camera.control_mode = eng::SpectatorCamera::ControlMode::FPS;
+    eng::disable_cursor();
+
+    layer->main_fbo = Framebuffer::create();
+    layer->main_fbo.add_renderbuffer({RenderbufferType::DEPTH, window_size});
+    layer->main_fbo.add_color_attachment({.type = ColorAttachmentType::TEX_2D,
+                                          .format = TextureFormat::RGBA8,
+                                          .wrap = GL_CLAMP_TO_EDGE,
+                                          .min_filter = GL_LINEAR,
+                                          .mag_filter = GL_LINEAR,
+                                          .size = window_size,
+                                          .gen_minmaps = false});
+
+    layer->asset_pack = eng::AssetPack::create("default");
+
+    layer->scene = eng::Scene::create("New scene");
+
+    eng::Entity ent = layer->scene.spawn_entity("ent1");
+    ent.add_component<eng::MeshComp>().id = eng::AssetPack::CUBE_ID;
+    ent.add_component<eng::MaterialComp>().id =
+        eng::AssetPack::DEFAULT_MATERIAL;
+
+    ent = layer->scene.spawn_entity("light");
+    ent.get_component<eng::Transform>().position = glm::vec3(2.0f, 2.0f, 1.0f);
+    ent.add_component<eng::PointLight>().intensity = 3.0f;
+
+    return layer;
+}
+
+void EditorLayer::destroy() {
+    scene.destroy();
+    asset_pack.destroy();
+    main_fbo.destroy();
+}
+
+void EditorLayer::on_attach() {}
+
+void EditorLayer::on_detach() {}
+
+void EditorLayer::on_event(eng::Event &event) {
+    switch (event.type) {
+    case eng::EventType::KeyPressed:
+        if (event.key.key == eng::Key::Escape)
+            eng::Window::terminate();
+        else if (event.key.key == eng::Key::Q) {
+            camera.control_mode = eng::SpectatorCamera::ControlMode::FPS;
+            eng::disable_cursor();
+        } else if (event.key.key == eng::Key::E) {
+            camera.control_mode = eng::SpectatorCamera::ControlMode::TRACKBALL;
+            eng::enable_cursor();
+        }
+
+        break;
+    default:
+        break;
+    }
+}
+
+void EditorLayer::on_update(float ts) {
+    camera.update_with_input(ts);
+}
+
+void EditorLayer::on_tick(uint32_t tickrate) {}
+
+void EditorLayer::on_render() {
+    ImGuiViewport *viewport = ImGui::GetMainViewport();
+    ImGui::SetNextWindowPos(viewport->Pos);
+    ImGui::SetNextWindowSize(viewport->Size);
+    ImGui::SetNextWindowViewport(viewport->ID);
+    ImGui::PushStyleVar(ImGuiStyleVar_WindowRounding, 0.0f);
+    ImGui::PushStyleVar(ImGuiStyleVar_WindowBorderSize, 0.0f);
+    ImGui::PushStyleVar(ImGuiStyleVar_WindowPadding, ImVec2(0.0f, 0.0f));
+    ImGui::Begin("main_dockspace", nullptr,
+                 ImGuiWindowFlags_NoDocking | ImGuiWindowFlags_NoTitleBar |
+                     ImGuiWindowFlags_NoCollapse | ImGuiWindowFlags_NoResize |
+                     ImGuiWindowFlags_NoMove |
+                     ImGuiWindowFlags_NoBringToFrontOnFocus |
+                     ImGuiWindowFlags_NoNavFocus);
+    ImGui::PopStyleVar();
+    ImGui::PopStyleVar(2);
+
+    ImGuiID dockspace_id = ImGui::GetID("dockspace");
+    ImGui::DockSpace(dockspace_id, ImVec2(0.0f, 0.0f),
+                     ImGuiDockNodeFlags_PassthruCentralNode);
+
+    static bool first_time = true;
+    if (first_time) {
+        first_time = false;
+
+        ImGui::DockBuilderRemoveNode(dockspace_id);
+        ImGui::DockBuilderAddNode(dockspace_id,
+                                  ImGuiDockNodeFlags_PassthruCentralNode);
+        ImGui::DockBuilderSetNodeSize(dockspace_id, viewport->Size);
+
+        ImGuiID left = ImGui::DockBuilderSplitNode(
+            dockspace_id, ImGuiDir_Left, 0.2f, nullptr, &dockspace_id);
+        ImGuiID right = ImGui::DockBuilderSplitNode(
+            dockspace_id, ImGuiDir_Right, 0.2f, nullptr, &dockspace_id);
+
+        ImGui::DockBuilderDockWindow("dock_left", left);
+        ImGui::DockBuilderDockWindow("dock_main", dockspace_id);
+        ImGui::DockBuilderDockWindow("dock_right", right);
+        ImGui::DockBuilderFinish(dockspace_id);
+    }
+
+    ImGui::End();
+
+    ImGui::Begin("dock_left");
+    ImGui::Text("left");
+    ImGui::End();
+
+    ImGui::PushStyleVar(ImGuiStyleVar_WindowPadding, {0.0f, 0.0f});
+    ImGui::Begin("dock_main", nullptr,
+                 ImGuiWindowFlags_NoResize | ImGuiWindowFlags_NoCollapse |
+                     ImGuiWindowFlags_NoTitleBar |
+                     ImGuiWindowFlags_NoBringToFrontOnFocus);
+
+    ImVec2 avail_region = ImGui::GetContentRegionAvail();
+    ImGui::Image((ImTextureID)main_fbo.color_attachments[0].id, avail_region,
+                 {0.0f, 1.0f}, {1.0f, 0.0f});
+    ImGui::PopStyleVar();
+    ImGui::End();
+
+    ImGui::Begin("dock_right");
+    ImGui::Text("right");
+    ImGui::End();
+
+    glm::ivec2 avail_region_iv2 = {(int32_t)avail_region.x,
+                                   (int32_t)avail_region.y};
+    camera.viewport = avail_region_iv2;
+
+    main_fbo.bind();
+    main_fbo.resize_everything(avail_region_iv2);
+    main_fbo.bind_renderbuffer();
+    main_fbo.bind_color_attachment(0);
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+    glClearColor(0.33f, 0.33f, 0.33f, 1.0f);
+
+    eng::renderer::scene_begin(camera.camera_render_data(), asset_pack);
+
+    eng::ecs::RegistryView rview =
+        scene.registry.view<eng::Transform, eng::PointLight>();
+    for (eng::ecs::RegistryView::Entry &entry : rview.entity_entries) {
+        eng::Transform &transform = rview.get<eng::Transform>(entry);
+        eng::PointLight &light = rview.get<eng::PointLight>(entry);
+
+        eng::renderer::submit_point_light(transform.position, light);
+    }
+
+    rview =
+        scene.registry.view<eng::Transform, eng::MeshComp, eng::MaterialComp>();
+    for (eng::ecs::RegistryView::Entry &entry : rview.entity_entries) {
+        eng::Transform &transform = rview.get<eng::Transform>(entry);
+        eng::MeshComp &mesh = rview.get<eng::MeshComp>(entry);
+        eng::MaterialComp &mat = rview.get<eng::MaterialComp>(entry);
+
+        eng::renderer::submit_mesh(transform.to_mat4(), mesh.id, mat.id);
+    }
+
+    eng::renderer::scene_end();
+
+    main_fbo.unbind();
+}
---
+++ edi/src/layers.hpp
@@ -0,0 +1,45 @@
+#ifndef LAYER_HPP
+#define LAYER_HPP
+
+#include "eng/event.hpp"
+#include "eng/renderer/camera.hpp"
+#include "eng/renderer/opengl.hpp"
+#include "eng/scene/scene.hpp"
+#include "eng/window.hpp"
+#include <memory>
+
+struct Layer {
+    virtual ~Layer() = default;
+
+    virtual void on_attach() = 0;
+    virtual void on_detach() = 0;
+
+    virtual void on_event(eng::Event &event) = 0;
+    virtual void on_update(float ts) = 0;
+    virtual void on_tick(uint32_t tickrate) = 0;
+    virtual void on_render() = 0;
+};
+
+struct EditorLayer : public Layer {
+    virtual ~EditorLayer() = default;
+
+    static std::unique_ptr<Layer> create(const eng::WindowSpec &win_spec);
+    void destroy();
+
+    virtual void on_attach() override;
+    virtual void on_detach() override;
+
+    virtual void on_event(eng::Event &event) override;
+    virtual void on_update(float ts) override;
+    virtual void on_tick(uint32_t tickrate) override;
+    virtual void on_render() override;
+
+    eng::Scene scene;
+    eng::AssetPack asset_pack;
+
+    Framebuffer main_fbo;
+
+    eng::SpectatorCamera camera;
+};
+
+#endif // LAYER_HPP

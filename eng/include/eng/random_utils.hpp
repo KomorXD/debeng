a145a070ef94089000f0239be255b3d09dcce4e1
Paweł Kupczak <komorxd_t@proton.me>
2025-09-05 00:01:06 +0200

    eng: add Result type

--- eng/include/eng/random_utils.hpp
+++ eng/include/eng/random_utils.hpp
@@ -10,4 +10,20 @@
 void replace_all(std::string &source, const std::string &pattern,
                  const std::string &replacement);
 
+/* Generic error, either success or failure. */
+enum class GenericError {
+    NO_ERROR,
+    ERROR
+};
+
+/* Generic result type - not stored as an union, but as a basic struct of
+ * expected value type and error type. Defined error type should be able to
+ * communicate that no errors occured, so no-error, in which case we should be
+ * confident that the value is valid. */
+template<typename ValueType, typename ErrorType = GenericError>
+struct Result {
+    ValueType value;
+    ErrorType error;
+};
+
 #endif

50398171236c107aa27e18f55ddb253a8eb8839f
Paweł Kupczak <komorxd_t@proton.me>
2025-10-14 15:56:42 +0200

    eng, utils: rewrite transform_decompose to be a glm::decompose wrapper

--- eng/include/eng/random_utils.hpp
+++ eng/include/eng/random_utils.hpp
@@ -11,7 +11,7 @@
 void replace_all(std::string &source, const std::string &pattern,
                  const std::string &replacement);
 
-bool transform_decompose(const glm::mat4 &transform, glm::vec3 &translation,
+void transform_decompose(const glm::mat4 &transform, glm::vec3 &translation,
                          glm::vec3 &rotation, glm::vec3 &scale);
 
 /* Generic error, either success or failure. */
--- eng/src/random_utils.cpp
+++ eng/src/random_utils.cpp
@@ -1,4 +1,5 @@
 #include "eng/random_utils.hpp"
+#include "glm/gtc/quaternion.hpp"
 #include <sstream>
 #include <string>
 #include <fstream>
@@ -32,56 +33,15 @@ void replace_all(std::string &source, const std::string &pattern,
     }
 }
 
-bool transform_decompose(const glm::mat4 &transform, glm::vec3 &translation,
+void transform_decompose(const glm::mat4 &transform, glm::vec3 &translation,
                          glm::vec3 &rotation, glm::vec3 &scale) {
-    // From glm::decompose in matrix_decompose.inl
+    glm::quat orientation;
+    glm::vec3 dummy_skew;
+    glm::vec4 dummy_perspevtive;
 
-    using namespace glm;
-    using T = float;
+    bool success = glm::decompose(transform, scale, orientation, translation,
+                                  dummy_skew, dummy_perspevtive);
+    assert(success && "Couldn't decompose transform matrix");
 
-    mat4 local_matrix(transform);
-
-    // Normalize the matrix.
-    if (epsilonEqual(local_matrix[3][3], static_cast<float>(0), epsilon<T>()))
-        return false;
-
-    // First, isolate perspective.  This is the messiest.
-    if (epsilonNotEqual(local_matrix[0][3], static_cast<T>(0), epsilon<T>()) ||
-        epsilonNotEqual(local_matrix[1][3], static_cast<T>(0), epsilon<T>()) ||
-        epsilonNotEqual(local_matrix[2][3], static_cast<T>(0), epsilon<T>())) {
-        // Clear the perspective partition
-        local_matrix[0][3] = local_matrix[1][3] = local_matrix[2][3] =
-            static_cast<T>(0);
-        local_matrix[3][3] = static_cast<T>(1);
-    }
-
-    // Next take care of translation (easy).
-    translation = vec3(local_matrix[3]);
-    local_matrix[3] = vec4(0, 0, 0, local_matrix[3].w);
-
-    vec3 row[3];
-
-    // Now get scale and shear.
-    for (length_t i = 0; i < 3; ++i)
-        for (length_t j = 0; j < 3; ++j)
-            row[i][j] = local_matrix[i][j];
-
-    // Compute X scale factor and normalize first row.
-    scale.x = length(row[0]);
-    row[0] = detail::scale(row[0], static_cast<T>(1));
-    scale.y = length(row[1]);
-    row[1] = detail::scale(row[1], static_cast<T>(1));
-    scale.z = length(row[2]);
-    row[2] = detail::scale(row[2], static_cast<T>(1));
-
-    rotation.y = asin(-row[0][2]);
-    if (cos(rotation.y) != 0) {
-        rotation.x = atan2(row[1][2], row[2][2]);
-        rotation.z = atan2(row[0][1], row[0][0]);
-    } else {
-        rotation.x = atan2(-row[2][0], row[1][1]);
-        rotation.z = 0;
-    }
-
-    return true;
+    rotation = glm::eulerAngles(orientation);
 }

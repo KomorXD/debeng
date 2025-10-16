272fef476e0b75fb7e5621f30518944962cb9658
Paweł Kupczak <komorxd_t@proton.me>
2025-10-16 18:26:43 +0200

    eng, scene: comments describing entity storage decision

--- eng/include/eng/scene/scene.hpp
+++ eng/include/eng/scene/scene.hpp
@@ -25,8 +25,20 @@ struct Scene {
 
     std::string name;
     ecs::Registry registry;
+
+    /* Entities are sorted in a hierarchical order - in other words,
+     * they are ordered in a way that we would encounter them if they were
+     * stored in a tree and we traversed it by depth.
+     * The reason to make it a vector instead of a tree is to optimize the most
+     * common use case: iterating over all of them (for UI rendering and
+     * updating global transforms). That way we iterate in hierarchical order
+     * and we get cache benefits. The price for that is more expensive
+     * hierarchy-changing operations (like changing parent), but it's not done
+     * every frame. */
     std::vector<Entity> entities;
 
+    /* For fast entity access. Be aware that changing the hierarchy might
+     * change indices, so you might need to fetch it again. */
     std::map<ecs::EntityID, int32_t> id_to_index;
 };
 
